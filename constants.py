6ab8f11ab0b60cf9e4bbb480e82b62b6e8f7c553
laightonlinux <bacalso.ton2x@gmail.com>
2025-01-11 22:46:45 +0800

    added rotation for I,J, and L piece
    - also added another game over condition. When a block is outside of bounds, then it is game over

--- constants.py
+++ constants.py
@@ -17,10 +17,33 @@ class PieceType(Enum):
     Z_PIECE = 3
     L_PIECE = 4
     J_PIECE = 5
+    O_PIECE = 6
 
 T_PIECE_COLOR = (233, 5, 233)
 I_PIECE_COLOR = (4, 233, 233)
-L_PIECE_COLOR = (233, 153, 4)
 S_PIECE_COLOR = (4, 233, 6)
 Z_PIECE_COLOR = (232, 5, 4)
+L_PIECE_COLOR = (233, 153, 4)
 J_PIECE_COLOR = (5, 5, 233)
+O_PIECE_COLOR = (233, 233, 4)
+
+ROTATION_OFFSETS = {
+    PieceType.I_PIECE.value: [
+        [(1, -2), (0, -1), (-1, 0), (-2, 1)],  # 0 degrees
+        [(-1, 1), (0, 0), (1, -1), (2, -2)],  # 90 degrees
+        [(2, -1), (1, 0), (0, 1), (-1, 2)],  # 180 degrees
+        [(-2, 2), (-1, 1), (0, 0), (1, -1)]  # 270 degrees
+    ],
+    PieceType.J_PIECE.value: [
+        [(-2, 0), (-1, 1), (0, 0), (1, -1)],
+        [(0, -2), (-1, -1), (0, 0), (1, 1)],
+        [(2, 0), (1, -1), (0, 0), (-1, 1)],
+        [(0, 2), (1, 1), (0, 0), (-1, -1)]
+    ],
+    PieceType.L_PIECE.value: [
+        [(1, -1), (0, 0), (-1, 1), (0, 2)],
+        [(1, 1), (0, 0), (-1, -1), (-2, 0)],
+        [(-1, 1), (0, 0), (1, -1), (0, -2)],
+        [(-1, -1), (0, 0), (1, 1), (2, 0)]
+    ]
+}
