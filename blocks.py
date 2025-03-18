1e4ff9efe5be8df08e82fa7ae373fae3797e6a88
bakaraw <bacalso.ton2x@gmail.com>
2025-03-18 13:10:53 +0800

    added timer text
    - also fixed some bugs in dropping the pieces

--- blocks.py
+++ blocks.py
@@ -1,11 +1,15 @@
 import pygame
-from constants import *
+from constants import UNIT, STROKE_WIDTH, PIECE_COLORS, \
+    BLOCKS_INITAL_POSITION, PIECE_INITIAL_X_OFFSET, ROTATION_OFFSETS
+
 
 class Block:
+
     def __init__(self, initial_pos, color) -> None:
         self.display_surface = pygame.display.get_surface()
         self.initial_pos = initial_pos
-        self.rect = pygame.Rect(self.initial_pos[0], self.initial_pos[1], UNIT, UNIT)
+        self.rect = pygame.Rect(
+            self.initial_pos[0], self.initial_pos[1], UNIT, UNIT)
         self.color = color
         self.stroke_color = (0, 0, 0)
         self.stroke_width = STROKE_WIDTH
@@ -13,19 +17,21 @@ class Block:
 
     def draw(self):
         pygame.draw.rect(self.display_surface, self.color, self.rect)
-        pygame.draw.rect(self.display_surface, self.stroke_color, self.rect, self.stroke_width)
-        
-    def move_upward(self, steps = 1):
+        pygame.draw.rect(self.display_surface, self.stroke_color,
+                         self.rect, self.stroke_width)
+
+    def move_upward(self, steps=1):
         self.rect.y -= steps * UNIT
 
-    def move_downward(self, steps = 1):
+    def move_downward(self, steps=1):
         self.rect.y += steps * UNIT
 
-    def move_left(self, steps = 1):
+    def move_left(self, steps=1):
         self.rect.x -= steps * UNIT
 
-    def move_right(self, steps = 1):
+    def move_right(self, steps=1):
         self.rect.x += steps * UNIT
+
 
 class Piece:
     def __init__(self, piece_type, initial_pos, game_board_rect, all_blocks, center=True) -> None:
@@ -47,16 +53,19 @@ class Piece:
     def generate_piece(self):
         blocks_inital_position = BLOCKS_INITAL_POSITION[self.piece_type]
         for pos in blocks_inital_position:
-            x = self.x + UNIT * pos[0] + (PIECE_INITIAL_X_OFFSET[self.piece_type] if self.center else 0)
+            x = self.x + UNIT * \
+                pos[0] + (PIECE_INITIAL_X_OFFSET[self.piece_type]
+                          if self.center else 0)
             y = self.y + UNIT * pos[1]
-            self.blocks.append(Block((x, y), self.color)) 
+            self.blocks.append(Block((x, y), self.color))
 
     def draw(self):
         for block in self.blocks:
             block.draw()
 
         for block in self.blocks:
-            pygame.draw.rect(self.display_surface, block.stroke_color, block.rect, block.stroke_width)
+            pygame.draw.rect(self.display_surface,
+                             block.stroke_color, block.rect, block.stroke_width)
 
     def update(self):
         if not self.check_collision(0, UNIT):
@@ -65,14 +74,13 @@ class Piece:
         else:
             self.reached_bottom = True
 
-
     def move_left(self):
-        if not self.check_collision(-UNIT, 0):
+        if not self.check_collision(-UNIT, 0) and not self.reached_bottom:
             for block in self.blocks:
                 block.move_left()
 
     def move_right(self):
-        if not self.check_collision(UNIT, 0):
+        if not self.check_collision(UNIT, 0) and not self.reached_bottom:
             for block in self.blocks:
                 block.move_right()
 
@@ -92,14 +100,14 @@ class Piece:
         offsets = ROTATION_OFFSETS[self.piece_type][self.rotate_position]
         for block, (dx, dy) in zip(self.blocks, offsets):
             block.rect.x += dx * UNIT
-            block.rect.y += dy * UNIT 
+            block.rect.y += dy * UNIT
 
         # check new blocks position if its within bounds and collision with other blocks
         collided = False
         for block in self.blocks:
 
             # this logic allow rotation of the pieces when close to the game board boundaries
-            if block.rect.x < self.game_board_rect.x :
+            if block.rect.x < self.game_board_rect.x:
                 for b in self.blocks:
                     b.move_right()
             elif block.rect.x >= self.game_board_rect.x + self.game_board_rect.width:
@@ -119,9 +127,3 @@ class Piece:
                 block.rect.x -= dx * UNIT
                 block.rect.y -= dy * UNIT
             self.rotate_position = (self.rotate_position - 1) % 4
-
-
-
-
-
-
